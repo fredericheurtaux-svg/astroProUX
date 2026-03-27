55c07ef1688a2475efc03d9f80f40a3a6115d663
Frederic Heurtaux <fredericheurtaux@MacBook-FHX.local>
2026-03-27 09:42:27 +0100

    Align Astro 6 and Node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
 export default defineConfig({
+  output: 'server',
   adapter: node({
     mode: 'standalone',
   }),
+  server: {
+    host: true,
+  },
 });
